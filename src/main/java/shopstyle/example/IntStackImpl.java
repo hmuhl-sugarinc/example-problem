70f725b26b586ec78a1242412fac0108db0b348c
Hayden Muhl <hmuhl@popsugar.com>
2015-08-17 17:14:55 -0700

    Skeleton IntStackImpl

--- src/main/java/shopstyle/example/IntStackImpl.java
+++ src/main/java/shopstyle/example/IntStackImpl.java
@@ -1,7 +1,24 @@
 package shopstyle.example;
 
-public class IntStackImpl {
+public class IntStackImpl implements IntStack {
 
+    public boolean isEmpty() {
+        // TODO: Implement this.
+        return true;
+    }
 
+    public int peek() {
+        // TODO: Implement this.
+        return 0;
+    }
+
+    public int pop() {
+        // TODO: Implement this.
+        return 0;
+    }
+
+    public void push(int val) {
+        // TODO: Implement this.
+    }
 
 }
