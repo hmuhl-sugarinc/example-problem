0613ed15fec1c085fa8eaf8a50e09d226a57edf1
Hayden Muhl <hmuhl@popsugar.com>
2015-08-17 17:24:19 -0700

    Added comments to the interface

--- src/main/java/shopstyle/example/IntStack.java
+++ src/main/java/shopstyle/example/IntStack.java
@@ -2,12 +2,25 @@ package shopstyle.example;
 
 public interface IntStack {
 
+    /**
+     *  Returns true if the stack is empty, false otherwise.
+     */
     public boolean isEmpty();
 
+    /**
+     *  Returns the value on the top of the stack, without modifying the stack
+     */
     public int peek();
 
+    /**
+     *  Removes the top value from the stack and returns it.
+     */
     public int pop();
 
+    /**
+     *  Adds a value to the top of the stack. This may require increasing the internal storage
+     *  capacity of this object.
+     */
     public void push(int val);
 
 }
