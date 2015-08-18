360728c18656ff53fa04b990a81e647a7d163b0c
Hayden Muhl <hmuhl@popsugar.com>
2015-08-17 17:17:41 -0700

    Added tests

--- src/main/java/shopstyle/example/IntStackImpl.java
+++ src/main/java/shopstyle/example/IntStackImpl.java
@@ -2,6 +2,12 @@ package shopstyle.example;
 
 public class IntStackImpl implements IntStack {
 
+    private int[] data;
+
+    public IntStackImpl() {
+        data = new int[16];
+    }
+
     public boolean isEmpty() {
         // TODO: Implement this.
         return true;
--- src/test/java/shopstyle/IntStackImplTest.java
+++ src/test/java/shopstyle/IntStackImplTest.java
@@ -1,13 +1,57 @@
 package shopstyle.example;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class IntStackImplTest {
 
     @Test
-    public void testFoo() {
-        assertEquals(1, 1);
+    public void testEmptyStack() {
+        IntStack stack = new IntStackImpl();
+
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testNonEmptyStack() {
+        IntStack stack = new IntStackImpl();
+
+        stack.push(10);
+
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    public void testPeek() {
+        IntStack stack = new IntStackImpl();
+
+        stack.push(10);
+        assertEquals(10, stack.peek());
+
+        stack.push(20);
+        assertEquals(20, stack.peek());
+    }
+
+    @Test
+    public void testPop() {
+        IntStack stack = new IntStackImpl();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        assertEquals(30, stack.pop());
+        assertEquals(20, stack.pop());
+        assertEquals(10, stack.pop());
+    }
+
+    @Test
+    public void testManyPushes() {
+        IntStack stack = new IntStackImpl();
+
+        for (int i = 0; i < 10000; i++) {
+            stack.push(i);
+        }
     }
 }
